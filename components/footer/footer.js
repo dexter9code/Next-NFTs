3bb520afc3444587fafe714f214408653c419546
rudy <doomdexter07@gmail.com>
2022-09-14 00:46:11 +0530

    minor fixes for navbar and logo

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,10 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
 import styles from "./footer.module.css";
 
 const Footer = function (props) {
   return (
     <div className={styles.footer}>
       <div className={styles.footer__logo}>
-        <img src="/assets/logo/NEXT-NFTs-1.png" alt="logo-image" />
+        <img src="/assets/logo/NEXT NFTs-1_trans.png" alt="logo-image" />
       </div>
       <ul className={styles.footer__nav}>
         <li>about us</li>
