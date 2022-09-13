49893dab84bb7a4a33123684b476b42d47acb71f
rudy <doomdexter07@gmail.com>
2022-09-13 22:25:59 +0530

    added admin api and comp

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import { SessionProvider } from "next-auth/react";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <SessionProvider>
+      <SessionProvider session={pageProps.session}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
--- pages/admin/adddata.js
+++ pages/admin/adddata.js
@@ -1,7 +1,34 @@
 import AddData from "./../../components/addData/AddData";
+import { getSession } from "next-auth/react";
 
 const AddingNfts = function (props) {
   return <AddData />;
 };
 
 export default AddingNfts;
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+  if (!session) {
+    return {
+      redirect: {
+        destination: `/signin`,
+        permanent: false,
+      },
+    };
+  }
+
+  if (session.user.email !== "admin") {
+    return {
+      redirect: {
+        destination: `/signin`,
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {
+      session,
+    },
+  };
+}
