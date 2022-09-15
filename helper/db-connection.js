3796674d8339a8bf05d1d6ede0ec7e53110fe0b9
rudy <doomdexter07@gmail.com>
2022-09-15 23:19:45 +0530

    error in ENV

--- helper/db-connection.js
+++ helper/db-connection.js
@@ -1,8 +1,9 @@
 import { MongoClient } from "mongodb";
 
 export const connectionDb = async function () {
-  const connectionString = `mongodb+srv://${process.env.DB_ADMIN_NAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mcfpfpr.mongodb.net/${process.env.DB_COLLECTION}?retryWrites=true&w=majority`;
-  return await MongoClient.connect(connectionString);
+  return await MongoClient.connect(
+    `mongodb+srv://Mikey:myNameIs_Mikey@cluster0.mcfpfpr.mongodb.net/nft?retryWrites=true&w=majority`
+  );
 };
 
 export const insertIntoDocument = async function (client, collection, doc) {
